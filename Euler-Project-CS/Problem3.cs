79edc9969be34c89b572289b0be2e44b56fd82aa
RM-Terrell <terrellrmr@gmail.com>
2017-04-25 20:18:07 -0600

    Solution template and problem 3 start

---
+++ Euler-Project-CS/Problem3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?*/
+
+namespace Euler_Project_CS
+{
+    class Problem3
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine();
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Euler-Project-CS/Template.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Template for testing solutions, printing to console and keeping open until key is pressed*/
+
+namespace Euler_Project_CS
+{
+    class Template
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine();
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
