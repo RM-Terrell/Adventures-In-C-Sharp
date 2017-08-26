e2aab059962b98c3adce05b62d551e0efbbdd61c
RM-Terrell <terrellsystems@gmail.com>
2017-08-26 14:09:09 -0600

    Obligatory Hello World!

---
+++ MiscProblems/Basics/HelloWorld.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Back to basics! Print Hello World! to console and print a new line under it without resorting to just another console.writeline*/
+
+namespace MiscProblems.Basics
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello World!{0}Enviorment new line here", Environment.NewLine);
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
