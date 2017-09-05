53dd3aeced56236646ade043d68fe7b111b6e09f
RM-Terrell <terrellsystems@gmail.com>
2017-09-05 12:50:17 -0600

    Array Summation and String Searching problems

---
+++ MiscProblems/Basics/ArraySum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 
+ * Write a C# program to compute the sum of all the elements of an array of integers. Go to the editor
+Test Data:
+Array1: [1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1]
+Sample Output
+Sum: 69 
+*/
+
+namespace MiscProblems.Basics
+{
+    class ArraySum
+    {
+        static void Main(string[] args)
+        {
+            int[] Array1 = { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1 };
+
+            int ArraySum = 0;
+
+            foreach(int i in Array1)
+            {
+                ArraySum += i;
+            }
+
+            Console.WriteLine("Array sum is {0}", ArraySum);
+
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MiscProblems/Basics/StringSearch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 
+ * Write a C# program to check if a given string contains ‘w’ character between 1 and 3 times
+Test Data:
+Input a string (conatins at least one 'w' char) : w3resource
+Test the string contains 'w' character between 1 and 3 times:
+Sample Output
+True 
+*/
+
+//Going to tweak this a bit and prompt for user input for the string to be searched, the character they want to check. 
+
+namespace MiscProblems.Basics
+{
+    class StringSearch
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter string to be checked");
+            var inputString = Console.ReadLine();
+
+            Console.WriteLine("Enter substring to be searched");
+            var inputSubString = Console.ReadLine();
+
+            if(inputString.IndexOf(inputSubString) != -1)
+            {
+                Console.WriteLine("String contains the substring");
+            }
+
+            else
+            {
+                Console.WriteLine("Substring not contained in string");
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
