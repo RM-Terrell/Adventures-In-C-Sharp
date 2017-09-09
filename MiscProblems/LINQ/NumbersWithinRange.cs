df838d863f028d7c86631f3f0b78165180ab2499
RM-Terrell <terrellsystems@gmail.com>
2017-09-08 19:23:32 -0600

    Range and square limit problems

---
+++ MiscProblems/LINQ/NumbersWithinRange.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 
+ * Write a program in C# Sharp to find the psotive numbers from a list of numbers using two where conditions in LINQ Query.
+Expected Output:
+The numbers within the range of 1 to 11 are :
+1 3 6 9 10
+*/
+
+// Works for more than just pure positive numbers and runs off user input instead of hard coded
+
+
+namespace MiscProblems.LINQ
+{
+    class NumbersWithinRange
+    {
+        static void Main()
+        {
+            int[] sourceArray = { 1, 2, -4, -8, 10, 29, 4, 1, 1, -56, -7, 12, -9, -20, 50, 99 };
+            int lowerRange = 0;
+            int upperRange = 0;
+
+            Console.WriteLine("Enter lower range to query.");
+            lowerRange = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter upper range to query");
+            upperRange = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            var rangeQuery =
+                from nums in sourceArray
+                where nums <= upperRange
+                where nums >= lowerRange
+                select nums;
+
+            Console.WriteLine("Numbers within range of query are \n");
+            foreach(var nums in rangeQuery)
+            {
+                Console.Write("{0} ", nums);
+            }
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+                             
+        }
+    }
+}
---
+++ MiscProblems/LINQ/SquareOfElementQuery.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+ * 
+ * Write a program in C# Sharp to find the number of an array and the square of each number. Go to the editor
+Expected Output :
+{ Number = 9, SqrNo = 81 }
+{ Number = 8, SqrNo = 64 }
+{ Number = 6, SqrNo = 36 }
+{ Number = 5, SqrNo = 25 }
+
+ */
+// Problem instructions werent too clear to me at first. 
+// Going to interpret this as "Make a program that finds all values in an array with a square value larger than a certain value.
+
+namespace MiscProblems.LINQ
+{
+    class SquareOfElementQuery
+    {
+        static void Main()
+        {
+            int[] sourceArray = { 1, 2, -4, -8, 10, 29, 4, 1, 1, -56, -7, 12, -9, -20, 50, 99 };
+            int upperSquare = 0;
+
+            Console.WriteLine("Enter minimum square value of array element");
+            upperSquare = int.Parse(Console.ReadLine());
+
+            var squareQuery =
+                from nums in sourceArray
+                let square = nums * nums
+                where square > upperSquare
+                select new { nums, square };
+
+            foreach(var objects in squareQuery)
+            {
+                Console.WriteLine(objects);
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
