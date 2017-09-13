a9d70efa746a6304628ed8fda47559ddd8c7975b
RM-Terrell <terrellsystems@gmail.com>
2017-09-13 12:11:36 -0600

    Array input and value return problem

---
+++ MiscProblems/LINQ/InputArray.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Write a program in C# Sharp to Accept the members of a list through the keyboard and display the members more than a specific value. Go to the editor
+Test Data :
+Input the number of members on the List : 5
+Member 0 : 10
+Member 1 : 48
+Member 2 : 52
+Member 3 : 94
+Member 4 : 63
+
+Input the value above you want to display the members of the List : 59
+Expected Output :
+The numbers greater than 59 are :
+94
+63
+*/
+
+
+namespace MiscProblems.LINQ
+{
+    class InputArray
+    {
+        static void Main()
+        {
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t' };            
+
+            Console.WriteLine("Enter numbers to be placed in an array: ");
+            string text = Console.ReadLine();
+
+            Console.WriteLine("Input the value above which you want to display numbers: ");
+            int lowerLimit = int.Parse(Console.ReadLine());
+
+            string[] stringArray = text.Split(delimiterChars);
+            int[] numArray = new int[stringArray.Length];
+
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                numArray[i] = Int32.Parse(stringArray[i]);
+            }
+
+            var limitBoundQuery =
+                from vals in numArray
+                where vals > lowerLimit
+                select vals;
+
+            foreach (var items in limitBoundQuery)
+            {
+                Console.WriteLine(items);
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
