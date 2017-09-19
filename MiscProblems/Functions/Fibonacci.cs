c205ffe61fee6ca3d74beb4238e341bfb1375c33
RM-Terrell <terrellsystems@gmail.com>
2017-09-18 19:14:27 -0600

    exponent calculation and fibonacci sequence

---
+++ MiscProblems/Functions/Fibonacci.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Write a program in C# Sharp to create a function to display the n number Fibonacci sequence.
+Test Data :
+Input number of Fibonacci Series : 5
+Expected Output :
+The Fibonacci series of 5 numbers is :
+0 1 1 2 3
+*/
+
+namespace MiscProblems.Functions
+{
+    class Fibonacci
+    {
+        public static void Main()
+        {
+            int fibN = 0;
+
+            Console.WriteLine("How may elemenents of the Fibonacci sequence should be generated?");
+            fibN = int.Parse(Console.ReadLine());
+
+            int[] fibReadOut = calcFib(fibN);
+            foreach (int i in fibReadOut)
+            {
+                Console.WriteLine(i);                
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+
+        }
+
+        public static int[] calcFib(int fibN)
+        {
+            int[] fibCalced = new int[fibN];
+            fibCalced[0] = 0;
+            fibCalced[1] = 1;
+
+            for(int i=2; i<fibN; i++)
+            {
+                fibCalced[i] = fibCalced[i - 1] + fibCalced[i - 2];
+            }            
+            return fibCalced;
+        }
+    }
+}
---
+++ MiscProblems/Functions/PowerCalc.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ *Write a program in C# Sharp to create a function to calculate the result of raising an integer number to another.
+Test Data :
+Input Base number: 3
+Input the Exponent : 2
+Expected Output :
+So, the number 3 ^ (to the power) 2 = 9
+*/
+
+// Base C# has no operator for power functions but .NET does. Opted to use .NET solution
+
+namespace MiscProblems.Functions
+{
+    class PowerCalc
+    {
+        public static void Main()
+        {
+            double baseValue = 0;
+            double expValue = 0;
+            double result = 0;
+
+
+            Console.WriteLine("Enter base value: ");
+            baseValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter exponent value: ");
+            expValue = double.Parse(Console.ReadLine());
+
+            result = ExponentCalc(baseValue, expValue);
+
+            Console.WriteLine("Result is: {0}\nPress any key to exit", result);
+            Console.ReadKey();
+            
+        }
+
+        public static double ExponentCalc(double baseValue, double expValue)
+        {
+            double result = 0;
+
+            result = Math.Pow(baseValue, expValue);
+
+            return result;
+        }
+    }
+}
