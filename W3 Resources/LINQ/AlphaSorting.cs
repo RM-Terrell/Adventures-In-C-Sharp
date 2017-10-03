5b81150c788024606fe5b35d61a10a76e24ef2ac
RM-Terrell <terrellsystems@gmail.com>
2017-10-02 18:00:11 -0600

    alpha sort problem

---
+++ W3 Resources/LINQ/AlphaSorting.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* 
+ * Write a program in C# Sharp to arrange the distinct elements in the list in ascending order. 
+Expected Output :
+Biscuit
+Brade
+Butter
+Honey
+*/
+
+namespace W3Resources.LINQ
+{
+    class AlphaSorting
+    {
+        static void Main()
+        {
+            string[] citiesArr = { "Rome", "London", "Nairobi", "Boulder", "Zurich", "New Dehli", "Amsterdam", "Oslo", "Paris" };
+
+            var alphaSort = citiesArr.OrderBy(n => n);
+
+            foreach (var city in alphaSort)
+            {
+                Console.WriteLine(city);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
