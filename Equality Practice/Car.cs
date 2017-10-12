1abf2161f87a8ace6991770255c849a49cf118f9
RM-Terrell <terrellsystems@gmail.com>
2017-10-11 19:11:44 -0600

    Equality practice

---
+++ Equality Practice/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equality_Practice
+{
+    public /*struct*/ class Car 
+        // chaging this to a structure overrides reference equality behavior 
+        //because struct derives from system.valuetype. This auto behavior is a bit slow though
+    {
+        private string _name;
+        public string Name { get { return _name; } }
+
+        public Car(string name)
+        {
+            this._name = name;
+        }
+
+        public override string ToString()
+        {
+            return _name;
+        }
+    }
+}
---
+++ Equality Practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// "==" operator is part of C#. Equals method is part of the .NET framework. 
+
+namespace Equality_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car Impreza = new Car("Impreza");
+            Car Impreza2 = new Car("Impreza");
+
+            Car Mustang = new Car("Mustang");
+
+            Console.WriteLine(Impreza.Equals(Mustang));
+            Console.WriteLine(Impreza.Equals(Impreza2)); // Returns false because Equals operator checks reference by default
+            // Console.WriteLine(null.Equals(Impreza)); //This will throw and exception due to calling method on a null
+
+            Console.WriteLine(object.Equals(null, Impreza)); // first value can be null without throwing exception
+            Console.WriteLine(object.Equals(null, null)); // Returns true
+
+            Console.WriteLine(ReferenceEquals(Impreza, Impreza2)); // returns false b/c checks by reference. Cant override
+
+        }
+    }
+}
