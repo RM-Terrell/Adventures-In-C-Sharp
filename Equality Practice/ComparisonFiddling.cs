8f0d1ebb5de3b776b29bef2fd46d4bc161354f33
RM-Terrell <terrellsystems@gmail.com>
2017-10-14 11:12:52 -0600

    Structural equality

--- Equality Practice/ComparisonFiddling.cs
+++ Equality Practice/ComparisonFiddling.cs
@@ -13,19 +13,25 @@ namespace Equality_Practice
             string Subaru = "Subaru";
             string Ford = "Ford";
 
-            Console.WriteLine(Subaru.CompareTo(Ford)); // Compares based on alphabet order. 1 meaning "S" comes after "F"
-            Console.WriteLine(Subaru.CompareTo(Subaru));
-            Console.WriteLine(Ford.CompareTo(Subaru));
+            //Console.WriteLine(Subaru.CompareTo(Ford)); // Compares based on alphabet order. 1 meaning "S" comes after "F"
+            //Console.WriteLine(Subaru.CompareTo(Subaru));
+            //Console.WriteLine(Ford.CompareTo(Subaru));
 
-            Console.WriteLine("String".CompareTo("String")); // 0
-            Console.WriteLine("Atring".CompareTo("String")); // -1
-            Console.WriteLine("StringBigger".CompareTo("String")); // 1
-            Console.WriteLine("String".CompareTo("StringBigger")); // -1
+            //Console.WriteLine("String".CompareTo("String")); // 0
+            //Console.WriteLine("Atring".CompareTo("String")); // -1
+            //Console.WriteLine("StringBigger".CompareTo("String")); // 1
+            //Console.WriteLine("String".CompareTo("StringBigger")); // -1
 
+            Console.WriteLine(String.Compare("Subaru", "Ford", StringComparison.Ordinal)); // returns unicode difference
+            Console.WriteLine(String.Compare("subaru", "subaru", StringComparison.Ordinal)); 
+            Console.WriteLine(String.Compare("subaru", "SUBARU", StringComparison.Ordinal));
 
+            Console.WriteLine(String.Compare("subaru", "SUBARU", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine(String.Compare("Subaru", "subaru", StringComparison.CurrentCulture)); // Cultural ordering is different from ordinal
+            Console.WriteLine(String.Compare("Subaru", "subaru", StringComparison.Ordinal));
 
-
-
+            var areEqual = ("Subaru" == "Ford"); // Case sensitive, unicode comparison
+            Console.WriteLine(areEqual);
 
 
         }
---
+++ Equality Practice/Structural Equality/StructEqMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equality_Practice.Structural_Equality
+{
+    class StructEqMain
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = { "Hammer", "Saw", "Nail", "Drill" };
+            string[] arr2 = { "Hammer", "Saw", "Nail", "Drill" };
+
+            //Console.WriteLine(arr1 == arr2); // Returns false because reference equality
+            //Console.WriteLine(arr1.Equals(arr2)); // Returns false because reference equality
+
+            var arrayEq = (IStructuralEquatable)arr1;
+            bool structEqual = arrayEq.Equals(arr2, StringComparer.OrdinalIgnoreCase);
+            Console.WriteLine(structEqual);
+
+            // structural comparison
+            var arrayComp = (IStructuralComparable)arr1;
+            int structComp = arrayComp.CompareTo(arr2, StringComparer.OrdinalIgnoreCase);
+            Console.WriteLine(structComp);
+        }
+    }
+}
