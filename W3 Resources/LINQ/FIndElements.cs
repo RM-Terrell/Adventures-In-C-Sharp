371713414824dd18df083100e5880c3543b472e5
RM-Terrell <terrellsystems@gmail.com>
2017-10-23 15:23:21 -0600

    Simple struct

---
+++ W3 Resources/LINQ/FIndElements.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W3Resources.LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = {
+                                      "Monday",
+                                      "Tuesday",
+                                      "Wednesday",
+                                      "Thursday",
+                                      "Friday",
+                                      "Saturday",
+                                      "Sunday"
+                                  };
+            // index of Tuesday
+            int indexOfTues = Array.IndexOf(daysOfWeek, "Tuesday");
+            Console.WriteLine(indexOfTues);
+            Console.WriteLine();
+
+            // index of first day beginning with W
+            int indexOfW = Array.FindIndex(daysOfWeek, x => x[0] == 'W');
+            Console.WriteLine(indexOfW);
+            Console.WriteLine(daysOfWeek[indexOfW]);
+            Console.WriteLine();
+
+            // all days with 6 chars in name
+            string[] allWith6Chars = Array.FindAll(
+                daysOfWeek, x => x.Length == 6);
+            foreach (string item in allWith6Chars)
+                Console.WriteLine(item);
+        }
+    }
+}
---
+++ W3 Resources/Structs/SimpleStruct.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Write a program in C# Sharp to declare a simple structure. 
+Expected Output:
+
+Declare a simple structure :
+-------------------------------- 
+The sum of x and y is 40  
+*/
+
+namespace W3Resources.Structs
+{
+    struct SimpStruct
+    {
+        public int xValue;
+        public int yValue;
+    }
+    class SimpleStruct
+    {
+        public static void Main()
+        {
+            Console.Write("\n\nDeclare a simple structure :\n");
+            Console.Write("--------------------------------\n");
+            SimpStruct _simpStruct = new SimpStruct();
+
+            _simpStruct.xValue = 20;
+            _simpStruct.yValue = 30;
+            int sum = _simpStruct.yValue + _simpStruct.xValue;
+
+            Console.WriteLine("Sum of x and y is: {0}", sum);
+        }
+
+    }
+}
