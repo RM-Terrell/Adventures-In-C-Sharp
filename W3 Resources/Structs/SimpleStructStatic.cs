077b7484f5171bc429eaa8cd55cca770d1ebd475
RM-Terrell <terrellsystems@gmail.com>
2017-10-23 15:48:24 -0600

    simple struct statics

---
+++ W3 Resources/Structs/SimpleStructStatic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Write a program in C# Sharp to declare a simple structure. 
+Expected Output:
+
+Declare a simple structure :
+-------------------------------- 
+The sum of x and y is 40  
+*/
+
+namespace W3Resources.Structs
+{
+    struct SimpStruct
+    {
+        public static int xValue = 55;
+        public static int yValue = 33;
+    }
+    class SimpleStruct
+    {
+        public static void Main()
+        {
+            Console.Write("\n\nDeclare a simple structure :\n");
+            Console.Write("--------------------------------\n");
+
+            int sum = SimpStruct.xValue + SimpStruct.yValue;
+
+            Console.WriteLine("Sum of x and y is: {0}", sum);
+        }
+
+    }
+}
